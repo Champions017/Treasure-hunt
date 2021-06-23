9e1bb53cfb4536d6a5ed6b8fa26238c2a597ffbc
Champions017 <81851859+Champions017@users.noreply.github.com>
2021-06-23 14:17:36 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    textAlign(CENTER)
+    textFont("Castellar")
+    fill("white")
+    textSize(25)
+    text("R E V B A I L A", 180,50)
+    fill("cyan")
+    textSize(13)
+    text("Hint: Always changing, not constant!", 180,70)
+// add code for displaying rest of the names and the hints.
+    
+    textSize(25)
+    fill("white")
+    text("A T E D A S B A",180,250);
+    fill("cyan")
+    textSize(13)
+    text("Hint: Stores all information!",180,270)
+
+    textSize(25)
+    fill("white")
+    text("C U T N I F O N",780,150)
+    fill("cyan");
+    textSize(13);
+    text("Hint: Performs a particular task!",780,170)
+
+}
